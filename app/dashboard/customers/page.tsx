6c93efeb9013032fded1eee03384149b6d42accf
MykhailoK007 <kotmykhailo2000@gmail.com>
2024-02-16 00:53:20 +0200

    updated templates

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Customers(): React.ReactNode {
+  return <p>Customers Page</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Invoices(): React.ReactNode {
+  return <p>Invoices Page</p>;
+}
